67a98a5b4b2bfe8af1a4361ffea61fa1f9ca4d9c
Luis-Varona <lbvarona@mta.ca>
2025-03-19 15:01:52 -0300

    Added unit testing for pst_pairs
    Started developing (unfinished) unit tests for pst_pairs on the 5-hypercube. Also began configuring runtests.jl.

--- test/optimized_state_transfer.jl
+++ test/optimized_state_transfer.jl
@@ -1,8 +1,8 @@
 module TestOptimizedStateTransfer
 
 using Test
+using QuantumStateTransfer: optimized_state_transfer
 using Graphs: Graph
-using QuantumStateTransfer: OptimizedStateTransfer, optimized_state_transfer
 
 C4_adj = BitMatrix([0 1 0 1; # Cycle graph on 4 vertices (adj. matrix format)
                     1 0 1 0;
@@ -19,10 +19,10 @@ is_pst2 = true
 is_optimal_time1(time::Real) = time % 2π ≈ 3π / 4
 is_optimal_time2(time::Real) = time % 2π ≈ π / 2
 
-results1 = optimized_state_transfer(C4_adj, source, dest1)
-results2 = optimized_state_transfer(C4_graph, source, dest2)
-
-@testset "optimized_state_transfer" begin
+@time @testset "optimized_state_transfer" begin
+    results1 = optimized_state_transfer(C4_adj, source, dest1)
+    results2 = optimized_state_transfer(C4_graph, source, dest2)
+    
     @test results1.maximum_fidelity ≈ maximum_fidelity1
     @test results2.is_pst == is_pst2
     @test is_optimal_time1(results1.optimal_time)
